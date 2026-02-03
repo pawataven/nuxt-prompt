e4c4cf36f0c714199bc77af5e59e787bc20e13ae
pawataven <pawatwantrong@gmail.com>
2026-02-03 15:32:08 +0700

    ADD: Layouts/Default

---
+++ app/layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+  <div>
+    <header class="sticky z-50 w-full bg-white/95 backdrop-blur border-b border-slate-200">
+
+    </header>
+
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
