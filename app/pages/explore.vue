b987def618f554045144501579e648a5a1f7afb6
kookzaza15889 <kookzaza1236_m@hotmail.com>
2026-02-03 15:17:48 +0700

    first setting project

---
+++ app/pages/explore.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Explore Page</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// โค้ด Logic ของคุณ
+</script>
+
+<style scoped>
+</style>
---
+++ app/pages/profile.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Profile Page</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// โค้ด Logic ของคุณ
+</script>
+
+<style scoped>
+</style>
